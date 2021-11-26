24bec83a7e5155e151ba5d57a01fd549a95b8998
Xenon28082 <savva.roll@gmail.com>
2021-11-26 20:52:05 +0300

    Garbage

---
+++ dao/src/main/java/by/xenon28082/shop/dao/config/DatabaseConfig2.java
@@ -0,0 +1,31 @@
+package by.xenon28082.shop.dao.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DatabaseConfig2 {
+    public static String URL = "jdbc:postgresql://localhost:5432/mytestdb";
+
+    public static String DATABASE_NAME = "mytestdb";
+
+    public static String USERNAME = "postgres";
+
+    public static String PASSWORD = "password";
+
+    public static Connection getConnection() throws SQLException {
+        Connection connection;
+        Properties properties = new Properties();
+        properties.setProperty("user", USERNAME);
+        properties.setProperty("password", PASSWORD);
+
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        connection = DriverManager.getConnection(URL, properties);
+        return connection;
+    }
+}
